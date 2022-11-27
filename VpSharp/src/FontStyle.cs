1bf050eca0c5ee392b4544636d3353a909088889
Oliver Booth <me@olivr.me>
2022-11-27 17:17:32 +0000

    Add proprietary FontStyle enum
    Removes dependency on Windows-only API from System.Drawing

---
+++ VpSharp/src/FontStyle.cs
@@ -0,0 +1,22 @@
+﻿namespace VpSharp;
+
+/// <summary>
+///     An enumeration of font styles.
+/// </summary>
+public enum FontStyle
+{
+    /// <summary>
+    ///     Regular font.
+    /// </summary>
+    Regular = 0,
+
+    /// <summary>
+    ///     Bold font.
+    /// </summary>
+    Bold = 1,
+
+    /// <summary>
+    ///     Italic font.
+    /// </summary>
+    Italic = 2
+}
