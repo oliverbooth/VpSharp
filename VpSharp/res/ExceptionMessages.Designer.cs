1273cc2e0ac3fd4c56bf863f11287b5ca7c576f9
Oliver Booth <me@olivr.me>
2022-11-30 20:15:46 +0000

    Move ExceptionMessages to res namespace

--- VpSharp/res/ExceptionMessages.Designer.cs
+++ VpSharp/res/ExceptionMessages.Designer.cs
@@ -38,7 +38,7 @@ namespace VpSharp {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VpSharp.ExceptionMessages", typeof(ExceptionMessages).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VpSharp.res.ExceptionMessages", typeof(ExceptionMessages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
