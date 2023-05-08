4a3e790767ca6f8033a8ca89afd70b7424fbf4ec
Oliver Booth <me@olivr.me>
2023-05-08 17:29:39 +0100

    feat(commands): add RequireBotOwnerAttribute

---
+++ VpSharp.Commands/Attributes/ExecutionChecks/RequireBotOwnerAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace VpSharp.Commands.Attributes.ExecutionChecks;
+
+/// <summary>
+///     Specifies that this command can only be run by the user under whom this bot is authenticated.
+/// </summary>
+public sealed class RequireBotOwnerAttribute : PreExecutionCheckAttribute
+{
+    /// <inheritdoc />
+    protected internal override Task<bool> PerformAsync(CommandContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+        return Task.FromResult(context.Avatar.User.Id == context.Client.CurrentUser?.Id);
+    }
+}
