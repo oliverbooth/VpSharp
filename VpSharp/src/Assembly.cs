c8244678454b24b7dee81af05a8cdad8e694e974
Oliver Booth <me@olivr.me>
2023-05-25 19:29:31 +0100

    refactor: remove Assembly.cs, define attributes in csproj

--- VpSharp/src/Assembly.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: CLSCompliant(true)]
-[assembly: InternalsVisibleTo("VpSharp.Commands")]
-[assembly: InternalsVisibleTo("VpSharp.IntegrationTests")]
-[assembly: InternalsVisibleTo("VpSharp.Tests")]
