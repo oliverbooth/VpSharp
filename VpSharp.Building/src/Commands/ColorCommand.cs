3f3e4543afb2d6d1d17cad95b832587301af8cb7
Oliver Booth <git@olivr.me>
2024-12-20 17:10:16 +0000

    feat: add color command

---
+++ VpSharp.Building/src/Commands/ColorCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using VpSharp.Building.Commands.Converters;
+
+namespace VpSharp.Building.Commands;
+
+/// <summary>
+///     Represents the <c>color</c> command.
+/// </summary>
+[Command("color", ConverterType = typeof(ColorCommandConverter))]
+public sealed class ColorCommand : VirtualParadiseCommand
+{
+    /// <summary>
+    ///     Gets or sets the color value.
+    /// </summary>
+    /// <value>The color value.</value>
+    public Color Color { get; set; } = Color.Transparent;
+}
