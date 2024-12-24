dcf4d5b052dc910da48dc66461aaa793c17eba7e
Oliver Booth <git@olivr.me>
2024-12-24 16:14:19 +0000

    feat: add picture command

---
+++ VpSharp.Building.Tests/test/CommandConverters/Deserialization/PictureCommandTests.cs
@@ -0,0 +1,70 @@
+using VpSharp.Building.Commands;
+using VpSharp.Building.Triggers;
+
+namespace VpSharp.Building.Tests.CommandConverters.Deserialization;
+
+internal sealed class PictureCommandTests
+{
+    [Test]
+    public void CreatePicture_ShouldDeserialize_WithPictureUrl()
+    {
+        const string source = "create picture vmist.net/ptv/tv.php";
+        VirtualParadiseAction action = ActionSerializer.Deserialize(source);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(action.Triggers, Has.Count.EqualTo(1));
+            Assert.That(action.Triggers.OfType<CreateTrigger>(), Is.Not.Null);
+            Assert.That(action.Create.Commands, Has.Count.EqualTo(1));
+            Assert.That(action.Create.Commands[0], Is.InstanceOf<PictureCommand>());
+
+            var command = (PictureCommand)action.Create.Commands[0];
+            Assert.That(command.Picture, Is.EqualTo("vmist.net/ptv/tv.php"));
+            Assert.That(command.Update, Is.EqualTo(TimeSpan.Zero));
+            Assert.That(command.Tag, Is.Null);
+            Assert.That(command.ExecuteAs, Is.Null);
+        });
+    }
+
+    [Test]
+    public void CreatePicture_ShouldDeserialize_WithPictureUrlAndUpdateProperty()
+    {
+        const string source = "create picture vmist.net/ptv/tv.php update=10";
+        VirtualParadiseAction action = ActionSerializer.Deserialize(source);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(action.Triggers, Has.Count.EqualTo(1));
+            Assert.That(action.Triggers.OfType<CreateTrigger>(), Is.Not.Null);
+            Assert.That(action.Create.Commands, Has.Count.EqualTo(1));
+            Assert.That(action.Create.Commands[0], Is.InstanceOf<PictureCommand>());
+
+            var command = (PictureCommand)action.Create.Commands[0];
+            Assert.That(command.Picture, Is.EqualTo("vmist.net/ptv/tv.php"));
+            Assert.That(command.Update, Is.EqualTo(TimeSpan.FromSeconds(10)));
+            Assert.That(command.Tag, Is.Null);
+            Assert.That(command.ExecuteAs, Is.Null);
+        });
+    }
+
+    [Test]
+    public void CreatePicture_ShouldDeserialize_WithPictureUrlAndTagPropertyAndUpdateProperty()
+    {
+        const string source = "create picture vmist.net/ptv/tv.php update=10 tag=foo";
+        VirtualParadiseAction action = ActionSerializer.Deserialize(source);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(action.Triggers, Has.Count.EqualTo(1));
+            Assert.That(action.Triggers.OfType<CreateTrigger>(), Is.Not.Null);
+            Assert.That(action.Create.Commands, Has.Count.EqualTo(1));
+            Assert.That(action.Create.Commands[0], Is.InstanceOf<PictureCommand>());
+
+            var command = (PictureCommand)action.Create.Commands[0];
+            Assert.That(command.Picture, Is.EqualTo("vmist.net/ptv/tv.php"));
+            Assert.That(command.Update, Is.EqualTo(TimeSpan.FromSeconds(10)));
+            Assert.That(command.Tag, Is.EqualTo("foo"));
+            Assert.That(command.ExecuteAs, Is.Null);
+        });
+    }
+}
---
+++ VpSharp.Building/src/Commands/PictureCommand.cs
@@ -0,0 +1,36 @@
+﻿using VpSharp.Building.Annotations;
+
+namespace VpSharp.Building.Commands;
+
+/// <summary>
+///     Represents the <c>picture</c> command.
+/// </summary>
+[Command("picture")]
+public sealed class PictureCommand : VirtualParadiseCommand
+{
+    /// <summary>
+    ///     Gets or sets the picture name.
+    /// </summary>
+    /// <value>The picture name.</value>
+    [Parameter(0)]
+    public string Picture { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the tag to which the texture is applied.
+    /// </summary>
+    /// <value>The tag to which the texture is applied.</value>
+    [Property("tag")]
+    public string? Tag { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the time after which the texture is updated.
+    /// </summary>
+    /// <value>The time after which the texture is updated.</value>
+    public TimeSpan Update
+    {
+        get => TimeSpan.FromSeconds(UpdateRaw);
+        set => UpdateRaw = value.TotalSeconds;
+    }
+
+    [Property("update")] private double UpdateRaw { get; set; }
+}
