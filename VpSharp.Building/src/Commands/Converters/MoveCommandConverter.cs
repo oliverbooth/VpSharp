6e723a57cac4c3b7a2cb7bc6fcd4b0717a191780
Oliver Booth <git@olivr.me>
2024-12-24 18:37:15 +0000

    feat: add complete move command

---
+++ VpSharp.Building/src/Commands/Converters/MoveCommandConverter.cs
@@ -0,0 +1,58 @@
+namespace VpSharp.Building.Commands.Converters;
+
+/// <summary>
+///     Represents a command converter for the <see cref="MoveCommand" /> command.
+/// </summary>
+public sealed class MoveCommandConverter : CommandConverter<MoveCommand>
+{
+    /// <inheritdoc />
+    public override void Read(ref Utf8ActionReader reader, MoveCommand command, ActionSerializerOptions options)
+    {
+        Span<double> values = stackalloc double[3];
+        int count = 0;
+
+        while (count < 3 &&
+               reader.Read().Type != TokenType.None &&
+               double.TryParse(reader.CurrentToken.ValueSpan, out values[count]))
+        {
+            count++;
+        }
+
+        command.Movement = count switch
+        {
+            1 => new Vector3d(values[0], 0, 0),
+            2 => new Vector3d(values[0], values[1], 0),
+            3 => new Vector3d(values[0], values[1], values[2]),
+            _ => command.Movement
+        };
+    }
+
+    /// <inheritdoc />
+    public override void Write(Utf8ActionWriter writer, MoveCommand? command, ActionSerializerOptions options)
+    {
+        if (command is null)
+        {
+            return;
+        }
+
+        switch (command.Movement)
+        {
+            case { X: 0.0, Z: 0.0 }:
+                writer.Write(command.Movement.X);
+                break;
+
+            case { Z: 0.0 }:
+                writer.Write(command.Movement.X);
+                writer.Write(command.Movement.Y);
+                break;
+
+            default:
+                writer.Write(command.Movement.X);
+                writer.Write(command.Movement.Y);
+                writer.Write(command.Movement.Z);
+                break;
+        }
+
+        WriteProperties(writer, command, options);
+    }
+}
--- VpSharp.Building/src/Commands/MoveCommand.cs
+++ VpSharp.Building/src/Commands/MoveCommand.cs
@@ -1,16 +1,90 @@
 ﻿using System.Numerics;
+using VpSharp.Building.Annotations;
+using VpSharp.Building.Commands.Converters;
 
 namespace VpSharp.Building.Commands;
 
 /// <summary>
 ///     Represents the <c>move</c> command.
 /// </summary>
-[Command("move")]
+[Command("move", ConverterType = typeof(MoveCommandConverter))]
 public sealed class MoveCommand : VirtualParadiseCommand
 {
     /// <summary>
     ///     Gets or sets the move value.
     /// </summary>
     /// <value>The move value.</value>
-    public Vector3 Movement { get; set; } = Vector3.Zero;
+    [Parameter(0)]
+    public Vector3d Movement { get; set; } = Vector3d.Zero;
+
+    /// <summary>
+    ///     Gets or sets a value indicating whether this movement is along the object axis (as opposed to world axis).
+    /// </summary>
+    /// <value><see langword="true" /> if this movement is along the object axis; otherwise, <see langword="false" />.</value>
+    [Flag("ltm")]
+    public bool IsLocalAxis { get; set; }
+
+    /// <summary>
+    ///     Gets or sets a value indicating whether this movement loops.
+    /// </summary>
+    /// <value><see langword="true" /> if this movement loops; otherwise, <see langword="false" />.</value>
+    [Flag("loop")]
+    public bool IsLooping { get; set; }
+
+    /// <summary>
+    ///     Gets or sets a value indicating whether this movement is smooth.
+    /// </summary>
+    /// <value><see langword="true" /> if this movement is smooth; otherwise, <see langword="false" />.</value>
+    [Flag("smooth")]
+    public bool IsSmooth { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the offset to apply to universe time when synchronizing.
+    /// </summary>
+    /// <value>The offset.</value>
+    public TimeSpan Offset
+    {
+        get => TimeSpan.FromSeconds(OffsetRaw);
+        set => OffsetRaw = value.TotalSeconds;
+    }
+
+    /// <summary>
+    ///     Gets or sets a value indicating whether this movement resets after completing half a cycle.
+    /// </summary>
+    /// <value><see langword="true" /> if this movement resets; otherwise, <see langword="false" />.</value>
+    [Flag("reset")]
+    public bool ShouldReset { get; set; }
+
+    /// <summary>
+    ///     Gets or sets a value indicating whether this movement syncs.
+    /// </summary>
+    /// <value><see langword="true" /> if this movement syncs; otherwise, <see langword="false" />.</value>
+    [Flag("sync")]
+    public bool ShouldSync { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the duration of half of a cycle.
+    /// </summary>
+    /// <value>The duration of half of a cycle.</value>
+    public TimeSpan Time
+    {
+        get => TimeSpan.FromSeconds(TimeRaw);
+        set => TimeRaw = value.TotalSeconds;
+    }
+
+    /// <summary>
+    ///     Gets or sets the time before continuing movement after one half of a cycle.
+    /// </summary>
+    /// <value>The wait time.</value>
+    public TimeSpan Wait
+    {
+        get => TimeSpan.FromSeconds(WaitRaw);
+        set => WaitRaw = value.TotalSeconds;
+    }
+
+    [Property("offset")] private double OffsetRaw { get; set; }
+
+    [Property("time")] private double TimeRaw { get; set; } = 1.0;
+
+    [Property("wait")] private double WaitRaw { get; set; }
 }
