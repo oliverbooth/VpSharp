ce47283043376c29375596280b42f3336411d310
Oliver Booth <git@olivr.me>
2024-12-24 19:53:49 +0000

    feat: add camera command

---
+++ VpSharp.Building.Tests/test/CommandConverters/Deserialization/CameraCommandTests.cs
@@ -0,0 +1,67 @@
+﻿using VpSharp.Building.Commands;
+using VpSharp.Building.Triggers;
+
+namespace VpSharp.Building.Tests.CommandConverters.Deserialization;
+
+internal sealed class CameraCommandTests
+{
+    [Test]
+    public void CreateCamera_ShouldDeserialize_GivenTarget()
+    {
+        const string source = "create camera target=foo";
+        VirtualParadiseAction action = ActionSerializer.Deserialize(source);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(action.Triggers, Has.Count.EqualTo(1));
+            Assert.That(action.Triggers[0], Is.InstanceOf<CreateTrigger>());
+            Assert.That(action.Create.Commands, Has.Count.EqualTo(1));
+            Assert.That(action.Create.Commands[0], Is.InstanceOf<CameraCommand>());
+
+            var command = (CameraCommand)action.Create.Commands[0];
+            Assert.That(command.Target, Is.EqualTo("foo"));
+            Assert.That(command.Location, Is.Null);
+            Assert.That(command.ExecuteAs, Is.Null);
+        });
+    }
+
+    [Test]
+    public void CreateCamera_ShouldDeserialize_GivenLocation()
+    {
+        const string source = "create camera location=bar";
+        VirtualParadiseAction action = ActionSerializer.Deserialize(source);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(action.Triggers, Has.Count.EqualTo(1));
+            Assert.That(action.Triggers[0], Is.InstanceOf<CreateTrigger>());
+            Assert.That(action.Create.Commands, Has.Count.EqualTo(1));
+            Assert.That(action.Create.Commands[0], Is.InstanceOf<CameraCommand>());
+
+            var command = (CameraCommand)action.Create.Commands[0];
+            Assert.That(command.Target, Is.Null);
+            Assert.That(command.Location, Is.EqualTo("bar"));
+            Assert.That(command.ExecuteAs, Is.Null);
+        });
+    }
+
+    [Test]
+    public void CreateCamera_ShouldDeserialize_GivenTargetAndLocation()
+    {
+        const string source = "create camera location=bar target=foo";
+        VirtualParadiseAction action = ActionSerializer.Deserialize(source);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(action.Triggers, Has.Count.EqualTo(1));
+            Assert.That(action.Triggers[0], Is.InstanceOf<CreateTrigger>());
+            Assert.That(action.Create.Commands, Has.Count.EqualTo(1));
+            Assert.That(action.Create.Commands[0], Is.InstanceOf<CameraCommand>());
+
+            var command = (CameraCommand)action.Create.Commands[0];
+            Assert.That(command.Target, Is.EqualTo("foo"));
+            Assert.That(command.Location, Is.EqualTo("bar"));
+            Assert.That(command.ExecuteAs, Is.Null);
+        });
+    }
+}
---
+++ VpSharp.Building/src/Commands/CameraCommand.cs
@@ -0,0 +1,24 @@
+﻿using VpSharp.Building.Annotations;
+
+namespace VpSharp.Building.Commands;
+
+/// <summary>
+///     Represents the <c>camera</c> command.
+/// </summary>
+[Command("camera")]
+public sealed class CameraCommand : VirtualParadiseCommand
+{
+    /// <summary>
+    ///     Gets or sets the location value.
+    /// </summary>
+    /// <value>The location value.</value>
+    [Property("location")]
+    public string? Location { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the target value.
+    /// </summary>
+    /// <value>The target value.</value>
+    [Property("target")]
+    public string? Target { get; set; }
+}
