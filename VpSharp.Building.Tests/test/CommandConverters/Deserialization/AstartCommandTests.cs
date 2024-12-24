7face4e22056daf882c270d08b5294084df21aa1
Oliver Booth <git@olivr.me>
2024-12-24 17:06:10 +0000

    feat: add astart command

---
+++ VpSharp.Building.Tests/test/CommandConverters/Deserialization/AstartCommandTests.cs
@@ -0,0 +1,46 @@
+using VpSharp.Building.Commands;
+using VpSharp.Building.Triggers;
+
+namespace VpSharp.Building.Tests.CommandConverters.Deserialization;
+
+internal sealed class AstartCommandTests
+{
+    [Test]
+    public void ActivateAstart_ShouldDeserializeName_GivenName()
+    {
+        const string source = "activate astart foo";
+        VirtualParadiseAction action = ActionSerializer.Deserialize(source);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(action.Triggers, Has.Count.EqualTo(1));
+            Assert.That(action.Triggers[0], Is.InstanceOf<ActivateTrigger>());
+            Assert.That(action.Activate.Commands, Has.Count.EqualTo(1));
+            Assert.That(action.Activate.Commands[0], Is.InstanceOf<AstartCommand>());
+
+            var command = (AstartCommand)action.Activate.Commands[0];
+            Assert.That(command.Name, Is.EqualTo("foo"));
+            Assert.That(command.IsLooping, Is.False);
+            Assert.That(command.ExecuteAs, Is.Null);
+        });
+    }
+    [Test]
+    public void ActivateAstart_ShouldDeserializeNameAndLooping_GivenNameAndLooping()
+    {
+        const string source = "activate astart foo looping";
+        VirtualParadiseAction action = ActionSerializer.Deserialize(source);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(action.Triggers, Has.Count.EqualTo(1));
+            Assert.That(action.Triggers[0], Is.InstanceOf<ActivateTrigger>());
+            Assert.That(action.Activate.Commands, Has.Count.EqualTo(1));
+            Assert.That(action.Activate.Commands[0], Is.InstanceOf<AstartCommand>());
+
+            var command = (AstartCommand)action.Activate.Commands[0];
+            Assert.That(command.Name, Is.EqualTo("foo"));
+            Assert.That(command.IsLooping, Is.True);
+            Assert.That(command.ExecuteAs, Is.Null);
+        });
+    }
+}
